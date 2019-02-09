2b1841f549e4bebece76d5e1724dbb03911e702b
Jan Polzer <janman74@gmail.com>
2019-02-09 11:15:34 -0600

    Change dataset

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # machine_learning_hw1
+
+EECS 738 - Machine learning
+Income predicton
+
+Dataset from https://www.kaggle.com/uciml/adult-census-income
+
+We are trying to predict from census data if preson's income is over or under $50000
+
+Due date: 2/18/2019
+
+Jan Polzer and Ryan Duckworth
--- ml_income_prediction.py
+++ ml_income_prediction.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-EECS 738 - Machine learning
-Dataset from https://www.kaggle.com/uciml/red-wine-quality-cortez-et-al-2009#winequality-red.csv
-Due date: 2/18/2019
+@file:   ml_income_prediction.py
+@date:   2/6/2019
 @author: Jan Polzer and Ryan Duckworth
 """
 
@@ -30,6 +29,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # load dataset
-data = pd.read_csv('winequality-red.csv')
+data = pd.read_csv('adult.csv')
 
 
